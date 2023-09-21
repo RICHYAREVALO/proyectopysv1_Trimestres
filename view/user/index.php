516241a7a76b42271ec5f29a7396dd738479edd9
RICHYAREVALO <111027492+RICHYAREVALO@users.noreply.github.com>
2023-09-20 19:33:16 -0500

    se agrega la carpeta librerias para la libreria del pdf tambien se pone el pdf en las vista usuarios en la parte imprimir

--- view/user/index.php
+++ view/user/index.php
@@ -3,7 +3,7 @@
 session_start();
 
 //Validamos que existe un session ademas que el cargo que exista sea igual a 1 que es administrador
-if(!isset($_SESSION['cargo']) || $_SESSION['cargo'] !=2){
+if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 2) {
     /* 
     para redireccionar en php se utiliza header
     pero al ser datos enviados por la cabecera debe esjecutarse
@@ -12,60 +12,49 @@ if(!isset($_SESSION['cargo']) || $_SESSION['cargo'] !=2){
     */
     header('location: ../../index.php');
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css.user/menuempleado.css">
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    
-    
-
-    <title>Vista Usuario</title>
+    <link rel="icon" type="img/png" href="/img/menu.png" />
+    <link rel="stylesheet" type="text/css" href="css.user/menuempleado.css">
+    <title>Menu</title>
 </head>
+
 <body>
-<nav class="navbar">
+    <!-- ucfirst convierte la primera letra en mayusculo de una cadena -->
+    <nav class="navbar">
         <div class="container">
             <!-- ucfirst convierte la primera letra en mayusculo de una cadena -->
-           Usuario <br>
+            Aministrador <br>
             Hola <?php echo ucfirst($_SESSION['nombre']); ?>!
-           
-        <a href="../../controller/cerrarSesion.php">
-        <i class="fa-solid fa-right-from-bracket"></i> Cerrar sesion
-        </a>
-            
+            <h1>Pas y Salvo Beyonder</h1>
+            <a href="../../controller/cerrarSesion.php">
+                <i class="fa-solid fa-right-from-bracket"></i> Cerrar sesion
+            </a>
+
         </div>
     </nav>
-    <header>
-        <h1 id="titulo">Bienvenido a Beyonde Pas y Salvo</h1>
-        
-        <nav>
-            <ul>
-                <li><a href="solicitar.html" id="inicio">Solicitar PYS</a></li>
-                <li><a href="consultarusuario.html" id="acerca">Consultar PYS</a></li>
-                <li><a href="imprimir.html" id="servicios">Imprimir PYS</a></li>
-                <li><a href="contactenos.html" id="contacto">Contacto</a></li>
-            </ul>
-        </nav>
-    </header>
+    <h1>Paz y Salvo Beyonber</h1>
+    <p>
+    <h2>Empleado</h2>
 
-    <img class="imgen" src="../../img/beyonder333.jpg" alt="">
-    
+    <nav>
+        <a href="solicitar.php">Solicitar</a>
+        <a href="consultarusuario.html">Consultar</a>
+        <a href="imprimir.php">Imprimir</a>
+        <a href="contactenos.html">Contactenos</a>
+        <a href="index.html">Salir</a>
+        <div class="animation start-home"></div>
+    </nav>
 
-    <script src="js/user.js"></script>
+
 </body>
+
 </html>
-
-
-
-
-
-
